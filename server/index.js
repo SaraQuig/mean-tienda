cbc109e7202886227dd3836c75f355f5be5a5b55
SaraQuig <saraquig76@gmail.com>
2024-07-22 13:08:10 -0500

    Updated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { mongoose } = require('./config/db');
 const app = express();
 const port = process.env.PORT ||  3000;
 
+//Nombre de la app
 app.set('nombreApp', 'Tienda');
 app.set('puerto', port);
 
